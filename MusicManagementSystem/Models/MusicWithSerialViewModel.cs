8f96f4036c10a19ceff91ec10333a0a25b3607cd
kiki-time <kiki.peng0521@foxmail.com>
2026-01-15 00:16:39 +0800

    增加/修改功能及页面效果

--- MusicManagementSystem/Models/MusicWithSerialViewModel.cs
+++ MusicManagementSystem/Models/MusicWithSerialViewModel.cs
@@ -5,12 +5,10 @@ using System.Web;
 
 namespace MusicManagementSystem.Models
 {
-    /// <summary>
-    /// 带动态连续序号的歌曲视图模型
-    /// </summary>
+
     public class MusicWithSerialViewModel
     {
-        // 动态连续序号（页面展示用）
+        // 动态连续序号
         public int SerialNumber { get; set; }
         public int Id { get; set; }
         public string SongName { get; set; }
